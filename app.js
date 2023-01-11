476cd07ba956e82698df3dc19b28516e76200147
kochipek <ipekkochisarli@gmail.com>
2023-01-11 16:56:59 +0300

    gradient rainbow colors added

--- app.js
+++ app.js
@@ -10,16 +10,23 @@ const nums = '0123456789';
 
 const alphabet = katakana + latin + nums;
 
-const fontSize = 16;
+const fontSize = 20;
 const columns = canvas.width/fontSize;
 
 const rainDrops = Array.from({ length: columns }).fill(canvas.height);
+const gradient = context.createLinearGradient(100, 10, 1900, 1);
 
 const draw = () => {
-	context.fillStyle = 'rgba(0, 0, 0, 0.05)';
-	context.fillRect(0, 0, canvas.width, canvas.height);
-	
-	context.fillStyle = '#0F0';
+	context.fillStyle = 'rgba(1, 0, 0, 0.05)';
+	context.fillRect(0, 0, window.innerWidth, window.innerHeight);
+	gradient.addColorStop(0, 'red');
+	gradient.addColorStop(1 / 6, 'orange');
+	gradient.addColorStop(2 / 6, 'yellow');
+	gradient.addColorStop(3 / 6, 'green');
+	gradient.addColorStop(4 / 6, 'blue');
+	gradient.addColorStop(1 / 6, 'indigo');
+	gradient.addColorStop(1, 'violet');
+	context.fillStyle = gradient;
 	context.font = fontSize + 'px monospace';
 
 	for(let i = 0; i < rainDrops.length; i++)
@@ -34,4 +41,4 @@ const draw = () => {
 	}
 };
 
-setInterval(draw, 30);
+setInterval(draw, 40);
